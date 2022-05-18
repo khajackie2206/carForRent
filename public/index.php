2d3ee3709cf9b74182cd7f5396ff6bbaa4a02a10
khajackie2206 <khajackie2206>
2022-05-18 17:37:13 +0700

    feat: add register page and home page

--- public/index.php
+++ public/index.php
@@ -1,2 +1,10 @@
 <?php
+use Khanguyennfq\CarForRent\Application;
 require_once __DIR__.'/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include '../src/route.php';
+$app = new Application();
+$app->run();
