98668c475747e7fccd35f62b9cbf349abf77e855
khajackie2206 <khajackie2206>
2022-06-08 09:49:52 +0700

    fix: change index

--- public/index.php
+++ public/index.php
@@ -5,8 +5,6 @@ session_start();
 use Khanguyennfq\CarForRent\Core\Application;
 use Khanguyennfq\CarForRent\Core\Request;
 
-error_reporting(E_ALL);
-ini_set('display_errors',1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Application(new Request());
